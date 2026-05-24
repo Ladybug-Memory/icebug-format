a0b0e997a17f953196af38adeb4ba81805b3d6ef
Ally Heev <allyheev@gmail.com>
2026-05-24 21:08:20 +0530

    add icebug-mem converter

--- icebug_format/__init__.py
+++ icebug_format/__init__.py
@@ -1,3 +1,4 @@
 from icebug_format.cli import main
+from icebug_format.memory import IcebugMemGraph, convert_arrow_tables_to_csr
 
-__all__ = ["main"]
+__all__ = ["main", "IcebugMemGraph", "convert_arrow_tables_to_csr"]
